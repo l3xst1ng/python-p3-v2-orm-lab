e7902ea80e196cb514c0821983c61f8c72e00652
l3xst1ng <howdy.martt@gmail.com>
2024-06-09 23:02:10 +0300

    Completed python-p3-v2-orm-lab

--- lib/employee.py
+++ lib/employee.py
@@ -103,7 +103,7 @@ class Employee:
             WHERE id = ?
         """
         CURSOR.execute(sql, (self.name, self.job_title,
-                             self.department_id, self.id))
+            self.department_id, self.id))
         CONN.commit()
 
     def delete(self):
@@ -187,4 +187,13 @@ class Employee:
 
     def reviews(self):
         """Return list of reviews associated with current employee"""
+        from review import Review
+        sql = """
+            SELECT * FROM reviews
+            WHERE employee_id is?
+        """
+        CURSOR.execute(sql, (self.id,),)
+
+        rows = CURSOR.fetchall()
+        return [Review.instance_from_db(row) for row in rows]
         pass
